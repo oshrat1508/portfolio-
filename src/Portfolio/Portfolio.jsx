71897ebf02ab2f3cca9d99b8e9020b5f8d23d1c7
oshrat <oshbalay@gmail.com>
2023-01-24 13:16:14 +0200

    home

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import movieImage from "../assent/צילום מסך 2023-01-23 195335.png";
 import sharePhoto from '../assent/צילום מסך 2023-01-23 195506.png'
 export default function Portfolio() {
   return (
-    <section id="portfolio" className="w-full flex  items-center  flex-col pt-20 mb-32 ">
+    <section id="portfolio" className="w-full flex  items-center  flex-col pt-20 mb-10 ">
       <h1 className="text-xl font-medium underline mb-5"> my works</h1>
       <div className="md:w-3/4 flex  flex-col md:flex-row justify-around ">
           <Project projectName='movie' link={'https://oshratproject-client.onrender.com'} gitHub={'https://github.com/oshrat1508/movie-project-client'}  image={movieImage} />
--- src/Portfolio/Project.jsx
+++ src/Portfolio/Project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {AiOutlineLink ,AiFillGithub} from 'react-icons/ai'
 export default function Project(props) {
   return (
-    <div className="flex justify-between flex-wrap w-72 md:w-80">
+    <div className="flex justify-between flex-wrap mb-10 w-72 md:w-80">
       <div className="flex items-center w-full h-0  mt-2 relative top-40 z-10">
         <a href={props.link} target="_blank">  <AiOutlineLink  size={20} className='mr-3'/></a>
         <a href={props.gitHub} target="_blank">  <AiFillGithub size={20}/></a>
