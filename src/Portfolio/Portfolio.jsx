f9591597541e9dba13276dcc1e8491deea6f3173
oshrat <oshbalay@gmail.com>
2023-01-29 11:45:03 +0200

    add project

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Project from "./Project";
 import movieImage from "../assent/צילום מסך 2023-01-23 195335.png";
 import sharePhoto from '../assent/צילום מסך 2023-01-23 195506.png'
+import e_commerce from '../assent/צילום מסך 2023-01-29 114119.png'
 export default function Portfolio() {
   return (
     <section id="portfolio" className="w-full flex  items-center  flex-col pt-20 mb-10 ">
@@ -9,7 +10,7 @@ export default function Portfolio() {
       <div className="md:w-3/4 flex  flex-col md:flex-row justify-around ">
           <Project projectName='movie' link={'https://oshratproject-client.onrender.com'} gitHub={'https://github.com/oshrat1508/movie-project-client'}  image={movieImage} />
           <Project projectName='share photo'  link='https://picosh-client.onrender.com/' gitHub='https://github.com/oshrat1508/share-photo-client' image={sharePhoto}/>          
-          {/* <Project projectName='mall' image={'p'} /> */}
+          <Project projectName='share photo'  link='https://e-commerce-client-d1va.onrender.com' gitHub='https://github.com/oshrat1508/e-commerce-client' image={e_commerce}/>          
 
       </div>
     </section>
